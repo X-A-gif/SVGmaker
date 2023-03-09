a044631b32928c4246bd8ea9148f2d7a166b5588
enamorada <109630476+AlmaHakba@users.noreply.github.com>
2023-03-08 18:37:15 -0800

    added anwsers + function

--- index.js
+++ index.js
@@ -1,34 +1,48 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
 
-inquirer.prompt([
-  {
-    type: 'input',
-    name: 'text',
-    message: 'Enter up to three characters:',
-    validate: function (input) {
-      if (input.length > 3) {
-        return 'Please enter up to three characters';
-      }
-      return true;
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters for your logo:'
     },
-  },
-  {
-    type: 'input',
-    name: 'textColor',
-    message: 'Enter text color:',
-  },
-  {
-    type: 'list',
-    name: 'shape',
-    message: 'Choose a shape:',
-    choices: ['circle', 'triangle', 'square'],
-  },
-  {
-    type: 'input',
-    name: 'shapeColor',
-    message: 'Enter shape color:',
-  },
-])
-.then((answers) => {
- 
-});
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter a color keyword or hexadecimal number for the text:'
+    },
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape for your logo:',
+      choices: ['Circle', 'Triangle', 'Square']
+    },
+    {
+      type: 'input',
+      name: 'shapeColor',
+      message: 'Enter a color keyword or hexadecimal number for the shape:'
+    }
+  ])
+  .then(answers => {
+    
+    const logoText = answers.text;
+    const logoTextColor = answers.textColor;
+    const logoShape = answers.shape;
+    const logoShapeColor = answers.shapeColor;
+
+    const logoSvg = generateLogoSvg(logoText, logoTextColor, logoShape, logoShapeColor);
+
+    fs.writeFile('logo.svg', logoSvg, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log('Generated logo.svg');
+    });
+
+  })
+  .catch(error => {
+    console.log(error);
+  });
